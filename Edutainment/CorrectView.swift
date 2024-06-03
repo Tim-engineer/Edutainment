de94f6c230914594c331b945205cf0198dd21b21
Tim-engineer <matlakt@outlook.com>
2024-06-03 19:50:42 +0200

    changed many thing and it works but without the answer as the textfield

---
+++ Edutainment/CorrectView.swift
@@ -0,0 +1,18 @@
+//
+//  CorrectView.swift
+//  Edutainment
+//
+//  Created by Tim Matlak on 03/06/2024.
+//
+
+import SwiftUI
+
+struct CorrectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CorrectView()
+}
